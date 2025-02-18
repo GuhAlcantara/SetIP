4ef3d8cd0c2ca4ac34448373da8fdfd328e5f264
GuhAlcantara <gustavoamv@gmail.com>
2025-02-18 14:33:33 -0300

    Indetacao

--- Setip.c
+++ Setip.c
@@ -1,83 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
-
-int opcao;
-
-
-printf("Bem vindos(as)\n");
-printf("-----------------\n");
-
-do{
-printf("Escolha a opcao que deseja:\n");
-printf("1. Setar IP Estatico (USAR NA REDE *x*)\n"); //x = Nome da Rede do trabalho
-printf("2. Resetar IP Dinamico (usar FORA do Trabalho)\n");
-printf("3. Saiba Mais.\n");
-printf("4. Sair.\n");
-printf("Digite: ");
-scanf("%d", &opcao);
-
-
-
-
-
-switch (opcao)
+int main()
 {
-case 1 :
-            //name="Nome da Conexão" static [IP] [Máscara de Sub-rede] [Gateway Padrão]
-            // Foi escolhido xxx.xxx.x.120 pela cliente
-system("netsh interface ip set address name=Wi-Fi static 192.168.2.120 255.255.255.0 192.168.2.1");
 
-        //Resultado para a cliente
-printf("interface = Wi-Fi\n");      // Nome da interface (verifique com "netsh interface show interface")
-printf("ip = 192.168.2.120\n");        // Endereço IP configurado
-printf("Netmask = 255.255.255.0\n");   // Máscara de rede configurado
-printf("gateway = 192.168.2.1\n");     // Gateway padrão
-printf("dns = 8.8.8.8\n");             // Servidor DNS (ex: Google DNS)
+    int opcao;
 
-   
+    printf("Bem vindos(as)\n");
+    printf("-----------------\n");
 
-printf("IP estatico configurado com sucesso!\n\n");
+    do
+    {
+        printf("Escolha a opcao que deseja:\n");
+        printf("1. Setar IP Estatico (USAR NA REDE *x*)\n"); // x = Nome da Rede do trabalho
+        printf("2. Resetar IP Dinamico (usar FORA do Trabalho)\n");
+        printf("3. Saiba Mais.\n");
+        printf("4. Sair.\n");
+        printf("Digite: ");
+        scanf("%d", &opcao);
 
+        switch (opcao)
+        {
+        case 1:
+            // name="Nome da Conexão" static [IP] [Máscara de Sub-rede] [Gateway Padrão]
+            //  Foi escolhido xxx.xxx.x.120 pela cliente
+            system("netsh interface ip set address name=Wi-Fi static 192.168.2.120 255.255.255.0 192.168.2.1");
 
-    break;
+            // Resultado para a cliente
+            printf("interface = Wi-Fi\n");       // Nome da interface (verifique com "netsh interface show interface")
+            printf("ip = 192.168.2.120\n");      // Endereço IP configurado
+            printf("Netmask = 255.255.255.0\n"); // Máscara de rede configurado
+            printf("gateway = 192.168.2.1\n");   // Gateway padrão
+            printf("dns = 8.8.8.8\n");           // Servidor DNS (ex: Google DNS)
 
-case 2 :
+            printf("IP estatico configurado com sucesso!\n\n");
 
-system("netsh interface ip set address name=Wi-Fi dhcp"); //Volta o IP para dinamico
-system("netsh interface ip set dns name=Wi-fi dhcp"); // Volta o DNS para dinamico
+            break;
 
-        // Resultado para a cliente
-printf("IP dinamico configurado com sucesso!\n\n");
+        case 2:
 
-    break;
+            system("netsh interface ip set address name=Wi-Fi dhcp"); // Volta o IP para dinamico
+            system("netsh interface ip set dns name=Wi-fi dhcp");     // Volta o DNS para dinamico
 
-    case 3 :
-    printf("\nOpcao 3 : Saiba Mais \n -------------------- \n");
-    printf("Para entender melhor como o programa funciona\n");
-    printf("\nOpcao 1 - IP Estatico: O programa configura seu PC para usar um IP fixo, garantindo que ele sempre tenha o mesmo endereco de rede. Isso e util para servidores, acesso remoto e dispositivos que precisam de um IP constante.\n");
-    printf("\nOpcaoo 2 - IP Dinamico (Padrao de Fabrica): O programa retorna seu PC a configuracao original, permitindo que o roteador atribua um IP automaticamente. Isso e ideal para redes domesticas e uso comum, garantindo maior flexibilidade.\n");
+            // Resultado para a cliente
+            printf("IP dinamico configurado com sucesso!\n\n");
 
-    
-    break;
+            break;
 
-case 4 :
-    printf("Saindo...");
-    
-    break;
+        case 3:
+            printf("\nOpcao 3 : Saiba Mais \n -------------------- \n");
+            printf("Para entender melhor como o programa funciona\n");
+            printf("\nOpcao 1 - IP Estatico: O programa configura seu PC para usar um IP fixo, garantindo que ele sempre tenha o mesmo endereco de rede. Isso e util para servidores, acesso remoto e dispositivos que precisam de um IP constante.\n");
+            printf("\nOpcaoo 2 - IP Dinamico (Padrao de Fabrica): O programa retorna seu PC a configuracao original, permitindo que o roteador atribua um IP automaticamente. Isso e ideal para redes domesticas e uso comum, garantindo maior flexibilidade.\n");
 
-default:
+            break;
 
-printf("Opcao invalida , Tente novamente\n\n");
-    break;
-}
-} while (opcao !=4);
+        case 4:
+            printf("Saindo...");
 
+            break;
 
+        default:
 
-
-
+            printf("Opcao invalida , Tente novamente\n\n");
+            break;
+        }
+    } while (opcao != 4);
 
     return 0;
 }
